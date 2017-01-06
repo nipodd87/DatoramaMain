ee406603d5f2ad4da511a750f5060573a3c55323
USER\ravi.peddi <ravi.peddi@ignitionone.com>
2017-01-06 16:28:20 -0500

    Udated the ETL framework to handle different datatypes and fixes to verify all the columns in DB

--- src/main/java/com/ignitionone/datastorm/datorama/etl/RecordLevel.java
+++ src/main/java/com/ignitionone/datastorm/datorama/etl/RecordLevel.java
@@ -1,5 +1,6 @@
 package com.ignitionone.datastorm.datorama.etl;
 
+import com.amazonaws.services.dynamodbv2.xspec.S;
 import com.ignitionone.datastorm.datorama.sqlUtil.ExecuteSqlUtil;
 import com.ignitionone.datastorm.datorama.util.CommonUtil;
 import com.ignitionone.datastorm.datorama.util.ETLUtil;
@@ -15,18 +16,20 @@ public class RecordLevel {
     private String SQL_QUERY_NAME = "getrows";
     ExecuteSqlUtil execSqlUtil = new ExecuteSqlUtil();
 
-    ETLUtil etlUtil=new ETLUtil();
+    ETLUtil etlUtil = new ETLUtil();
+
 
     public void verifyTableData(String environment, Map<String, DestinationTable> mapSet, String srcSqlFile, String srcTable, String srcColumnNames, String destSqlFile, String destTable, String destColumnNames) throws Exception {
+        verifyTableData(SQL_QUERY_NAME, environment, mapSet, srcSqlFile, srcTable, srcColumnNames, destSqlFile, destTable, destColumnNames);
+    }
 
+    public void verifyTableData(String sqlQueryName, String environment, Map<String, DestinationTable> mapSet, String srcSqlFile, String srcTable, String srcColumnNames, String destSqlFile, String destTable, String destColumnNames) throws Exception {
 
         List<String> getColumnsSrc = execSqlUtil.getRows(srcSqlFile, environment, SQL_QUERY_NAME, "<<tableName>>", srcTable, "<<columnName>>", srcColumnNames);
         List<String> getColumnsDist = execSqlUtil.getRows(destSqlFile, environment, SQL_QUERY_NAME, "<<tableName>>", destTable, "<<columnName>>", destColumnNames);
 
         etlUtil.verifySrcDestinationData(mapSet, getColumnsSrc, getColumnsDist);
     }
-
-
 
 }
 
