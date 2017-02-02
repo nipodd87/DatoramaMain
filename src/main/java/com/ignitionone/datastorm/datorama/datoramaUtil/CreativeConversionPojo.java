01f5be7a52408ad16fec305ba9dab1e1ed98a198
KARTHIK INUGANTI <karthik.inuganti@ignitionone.com>
2017-02-02 12:13:12 -0500

    Company and creatve Conversion Pojo classes

---
+++ src/main/java/com/ignitionone/datastorm/datorama/datoramaUtil/CreativeConversionPojo.java
@@ -0,0 +1,245 @@
+package com.ignitionone.datastorm.datorama.datoramaUtil;
+
+/**
+ * Created by karthik.inuganti on 2/2/2017.
+ */
+public class CreativeConversionPojo {
+    private String date;
+    private String buId;
+    private String campaignId;
+    private String campaignName;
+    private String campaignFlightDateStart;
+    private String campaignFlightDateEnd;
+    private String accountManagerId;
+    private String campaignStatus;
+    private String advertiserSourceId;
+    private String advertiserSourceName;
+    private String campaignTargetId;
+    private String campaignTargetName;
+    private String campaignTargetFlightDateStart;
+    private String campaignTargetFlightDateEnd;
+    private String campaignTargetStatus;
+    private String creativeId;
+    private String creativeName;
+    private String creativeMessageId;
+    private String creativeMessageName;
+    private String adserverPlacementId;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getBuId() {
+        return buId;
+    }
+
+    public void setBuId(String buId) {
+        this.buId = buId;
+    }
+
+    public String getCampaignId() {
+        return campaignId;
+    }
+
+    public void setCampaignId(String campaignId) {
+        this.campaignId = campaignId;
+    }
+
+    public String getCampaignName() {
+        return campaignName;
+    }
+
+    public void setCampaignName(String campaignName) {
+        this.campaignName = campaignName;
+    }
+
+    public String getCampaignFlightDateStart() {
+        return campaignFlightDateStart;
+    }
+
+    public void setCampaignFlightDateStart(String campaignFlightDateStart) {
+        this.campaignFlightDateStart = campaignFlightDateStart;
+    }
+
+    public String getCampaignFlightDateEnd() {
+        return campaignFlightDateEnd;
+    }
+
+    public void setCampaignFlightDateEnd(String campaignFlightDateEnd) {
+        this.campaignFlightDateEnd = campaignFlightDateEnd;
+    }
+
+    public String getAccountManagerId() {
+        return accountManagerId;
+    }
+
+    public void setAccountManagerId(String accountManagerId) {
+        this.accountManagerId = accountManagerId;
+    }
+
+    public String getCampaignStatus() {
+        return campaignStatus;
+    }
+
+    public void setCampaignStatus(String campaignStatus) {
+        this.campaignStatus = campaignStatus;
+    }
+
+    public String getAdvertiserSourceId() {
+        return advertiserSourceId;
+    }
+
+    public void setAdvertiserSourceId(String advertiserSourceId) {
+        this.advertiserSourceId = advertiserSourceId;
+    }
+
+    public String getAdvertiserSourceName() {
+        return advertiserSourceName;
+    }
+
+    public void setAdvertiserSourceName(String advertiserSourceName) {
+        this.advertiserSourceName = advertiserSourceName;
+    }
+
+    public String getCampaignTargetId() {
+        return campaignTargetId;
+    }
+
+    public void setCampaignTargetId(String campaignTargetId) {
+        this.campaignTargetId = campaignTargetId;
+    }
+
+    public String getCampaignTargetName() {
+        return campaignTargetName;
+    }
+
+    public void setCampaignTargetName(String campaignTargetName) {
+        this.campaignTargetName = campaignTargetName;
+    }
+
+    public String getCampaignTargetFlightDateStart() {
+        return campaignTargetFlightDateStart;
+    }
+
+    public void setCampaignTargetFlightDateStart(String campaignTargetFlightDateStart) {
+        this.campaignTargetFlightDateStart = campaignTargetFlightDateStart;
+    }
+
+    public String getCampaignTargetFlightDateEnd() {
+        return campaignTargetFlightDateEnd;
+    }
+
+    public void setCampaignTargetFlightDateEnd(String campaignTargetFlightDateEnd) {
+        this.campaignTargetFlightDateEnd = campaignTargetFlightDateEnd;
+    }
+
+    public String getCampaignTargetStatus() {
+        return campaignTargetStatus;
+    }
+
+    public void setCampaignTargetStatus(String campaignTargetStatus) {
+        this.campaignTargetStatus = campaignTargetStatus;
+    }
+
+    public String getCreativeId() {
+        return creativeId;
+    }
+
+    public void setCreativeId(String creativeId) {
+        this.creativeId = creativeId;
+    }
+
+    public String getCreativeName() {
+        return creativeName;
+    }
+
+    public void setCreativeName(String creativeName) {
+        this.creativeName = creativeName;
+    }
+
+    public String getCreativeMessageId() {
+        return creativeMessageId;
+    }
+
+    public void setCreativeMessageId(String creativeMessageId) {
+        this.creativeMessageId = creativeMessageId;
+    }
+
+    public String getCreativeMessageName() {
+        return creativeMessageName;
+    }
+
+    public void setCreativeMessageName(String creativeMessageName) {
+        this.creativeMessageName = creativeMessageName;
+    }
+
+    public String getAdserverPlacementId() {
+        return adserverPlacementId;
+    }
+
+    public void setAdserverPlacementId(String adserverPlacementId) {
+        this.adserverPlacementId = adserverPlacementId;
+    }
+
+    public String getAdserverPlacementName() {
+        return adserverPlacementName;
+    }
+
+    public void setAdserverPlacementName(String adserverPlacementName) {
+        this.adserverPlacementName = adserverPlacementName;
+    }
+
+    public String getIntegrationId() {
+        return integrationId;
+    }
+
+    public void setIntegrationId(String integrationId) {
+        this.integrationId = integrationId;
+    }
+
+    public String getIntegrationName() {
+        return integrationName;
+    }
+
+    public void setIntegrationName(String integrationName) {
+        this.integrationName = integrationName;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    public void setCurrencyCode(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+
+    public String getClickBasedConversions() {
+        return clickBasedConversions;
+    }
+
+    public void setClickBasedConversions(String clickBasedConversions) {
+        this.clickBasedConversions = clickBasedConversions;
+    }
+
+    public String getImpressionBasedConversions() {
+        return impressionBasedConversions;
+    }
+
+    public void setImpressionBasedConversions(String impressionBasedConversions) {
+        this.impressionBasedConversions = impressionBasedConversions;
+    }
+
+    private String adserverPlacementName;
+    private String integrationId;
+    private String integrationName;
+    private String currencyCode;
+    private String clickBasedConversions;
+    private String impressionBasedConversions;
+
+
+
+}
