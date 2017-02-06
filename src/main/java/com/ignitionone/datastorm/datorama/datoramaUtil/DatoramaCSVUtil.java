83e88479cdb66ef3a84b30867fad061e83ce01b2
Nitin Poddar <nitin.poddar@ignitionone.com>
2017-02-06 13:02:34 -0500

    Commiting working code for Creative Delivery and Conversion for Sql to S3

--- src/main/java/com/ignitionone/datastorm/datorama/datoramaUtil/DatoramaCSVUtil.java
+++ src/main/java/com/ignitionone/datastorm/datorama/datoramaUtil/DatoramaCSVUtil.java
@@ -4,6 +4,8 @@ package com.ignitionone.datastorm.datorama.datoramaUtil;
 import au.com.bytecode.opencsv.CSVReader;
 import au.com.bytecode.opencsv.bean.ColumnPositionMappingStrategy;
 import au.com.bytecode.opencsv.bean.CsvToBean;
+import com.ignitionone.datastorm.datorama.model.ConversionMetrics;
+import com.ignitionone.datastorm.datorama.model.CreativeConversionBean;
 import com.ignitionone.datastorm.datorama.model.CreativeDeliveryBean;
 import com.ignitionone.datastorm.datorama.model.DeliveryMetrics;
 
@@ -18,16 +20,16 @@ import java.util.List;
 
 public class DatoramaCSVUtil {
 
-    public List<String> getCreativeDeliveryCSVData(String fileName, char separator, String delimiter) throws FileNotFoundException {
+    public static List<String> getCreativeDeliveryCSVData(String fileName, char separator, String delimiter) throws FileNotFoundException {
         ColumnPositionMappingStrategy<CreativeDeliveryBean> mapper = new ColumnPositionMappingStrategy<CreativeDeliveryBean>();
         mapper.setType(CreativeDeliveryBean.class);
-        String[] columns = new String[]{"Date", "BU_ID", "Campaign_ID", "Campaign_Name", "Campaign_Flightdate_Start", "Campaign_Flightdate_End", "Account_Manager_ID", "Campaign_Status",
-                "Advertiser_Source_ID", "Advertiser_Source_Name", "Campaign_Target_ID", "Campaign_Target_Name", "Campaign_Target_Flightdate_Start", "Campaign_Target_Flightdate_End", "Campaign_Target_Status", "Creative_ID",
-                "Creative_Name", "Creative_Message_ID", "Creative_Message_Name", "Adserver_Placement_ID", "Adserver_Placement_Name", "Integration_ID", "Integration_Name", "Currency_Code", "Impressions", "Clicks", "Cost"};
+        String[] columns = new String[]{"Date", "BUID", "CampaignID", "CampaignName", "CampaignFlightdateStart", "CampaignFlightdateEnd", "AccountManagerID", "CampaignStatus",
+                "AdvertiserSourceID", "AdvertiserSourceName", "CampaignTargetID", "CampaignTargetName", "CampaignTargetFlightdateStart", "CampaignTargetFlightdateEnd", "CampaignTargetStatus", "CreativeID",
+                "CreativeName", "CreativeMessageID", "CreativeMessageName", "AdserverPlacementID", "AdserverPlacementName", "IntegrationID", "IntegrationName", "CurrencyCode", "Impressions", "Clicks", "Cost"};
         mapper.setColumnMapping(columns);
 
         CsvToBean<CreativeDeliveryBean> csv = new CsvToBean<CreativeDeliveryBean>();
-        List<CreativeDeliveryBean> creativeDeliveryList = csv.parse(mapper, new CSVReader(new FileReader(System.getProperty("user.dir") + "/" + fileName + ".csv"), ','));
+        List<CreativeDeliveryBean> creativeDeliveryList = csv.parse(mapper, new CSVReader(new FileReader(System.getProperty("user.dir") + "/" + fileName), ','));
 
         List<String> creativeDeliveryModifiedList = new ArrayList<String>();
         for (int i = 0; i < creativeDeliveryList.size(); i++) {
@@ -100,13 +102,14 @@ public class DatoramaCSVUtil {
         int recordCount = 0;
         ColumnPositionMappingStrategy<CreativeDeliveryBean> mapper = new ColumnPositionMappingStrategy<CreativeDeliveryBean>();
         mapper.setType(CreativeDeliveryBean.class);
-        String[] columns = new String[]{"Date", "BU_ID", "Campaign_ID", "Campaign_Name", "Campaign_Flightdate_Start", "Campaign_Flightdate_End", "Account_Manager_ID", "Campaign_Status",
-                "Advertiser_Source_ID", "Advertiser_Source_Name", "Campaign_Target_ID", "Campaign_Target_Name", "Campaign_Target_Flightdate_Start", "Campaign_Target_Flightdate_End", "Campaign_Target_Status", "Creative_ID",
-                "Creative_Name", "Creative_Message_ID", "Creative_Message_Name", "Adserver_Placement_ID", "Adserver_Placement_Name", "Integration_ID", "Integration_Name", "Currency_Code", "Impressions", "Clicks", "Cost"};
+        String[] columns = new String[]{"Date", "BUID", "CampaignID", "CampaignName", "CampaignFlightdateStart", "CampaignFlightdateEnd", "AccountManagerID", "CampaignStatus",
+                "AdvertiserSourceID", "AdvertiserSourceName", "CampaignTargetID", "CampaignTargetName", "CampaignTargetFlightdateStart", "CampaignTargetFlightdateEnd", "CampaignTargetStatus", "CreativeID",
+                "CreativeName", "CreativeMessageID", "CreativeMessageName", "AdserverPlacementID", "AdserverPlacementName", "IntegrationID", "IntegrationName", "CurrencyCode", "Impressions", "Clicks", "Cost"};
         mapper.setColumnMapping(columns);
 
         CsvToBean<CreativeDeliveryBean> csv = new CsvToBean<CreativeDeliveryBean>();
         List<CreativeDeliveryBean> creativeDeliveryList = csv.parse(mapper, new CSVReader(new FileReader(System.getProperty("user.dir") + "/" + fileName), separator));
+        mapper.getColumnMapping();
         for (int i = 1; i < creativeDeliveryList.size(); i++) {
             totalImpression = totalImpression + Integer.parseInt(creativeDeliveryList.get(i).getImpressions());
             totalClicks = totalClicks + Integer.parseInt(creativeDeliveryList.get(i).getClicks());
@@ -118,5 +121,104 @@ public class DatoramaCSVUtil {
         metrics.setTotalCost(Math.floor(totalCost * 1000) / 1000);
         metrics.setRecordCount(recordCount);
         return metrics;
+    }
+
+    public static ConversionMetrics getCreativeConversionMeasurementTotal(String fileName, char separator) throws FileNotFoundException {
+
+        ConversionMetrics metrics = new ConversionMetrics();
+        int total_click_based_conversion = 0;
+        int total_view_based_conversion = 0;
+        int recordCount = 0;
+        ColumnPositionMappingStrategy<CreativeConversionBean> mapper = new ColumnPositionMappingStrategy<CreativeConversionBean>();
+        mapper.setType(CreativeConversionBean.class);
+        String[] columns = new String[]{"Date","BUID","CampaignID","CampaignName","CampaignFlightdateStart","CampaignFlightdateEnd","AccountManagerID",
+                "CampaignStatus","AdvertiserSourceID","AdvertiserSourceName","CampaignTargetID","CampaignTargetName","CampaignTargetFlightdateStart",
+                "CampaignTargetFlightdateEnd","CampaignTargetStatus","CreativeID","CreativeName","CreativeMessageID","CreativeMessageName",
+                "AdserverPlacementID","AdserverPlacementName","IntegrationID","IntegrationName","CurrencyCode","ClickBasedConversions","ImpressionBasedConversions"};
+        mapper.setColumnMapping(columns);
+
+        CsvToBean<CreativeConversionBean> csv = new CsvToBean<CreativeConversionBean>();
+        List<CreativeConversionBean> creativeConversionList = csv.parse(mapper, new CSVReader(new FileReader(System.getProperty("user.dir") + "/" + fileName), separator));
+        for (int i = 1; i <creativeConversionList.size(); i++) {
+            total_click_based_conversion = total_click_based_conversion + Integer.parseInt(creativeConversionList.get(i).getClickBasedConversions());
+            total_view_based_conversion = total_view_based_conversion + Integer.parseInt(creativeConversionList.get(i).getImpressionBasedConversions());
+            recordCount++;
+        }
+        metrics.setTotalClickBasedConversion(total_click_based_conversion);
+        metrics.setTotalViewBasedConversion(total_view_based_conversion);
+        metrics.setRecordCount(recordCount);
+        return metrics;
+    }
+
+    public static List<String> getCreativeConversionCSVData(String fileName, char separator, String delimiter) throws FileNotFoundException {
+        ColumnPositionMappingStrategy<CreativeConversionBean> mapper = new ColumnPositionMappingStrategy<CreativeConversionBean>();
+        mapper.setType(CreativeConversionBean.class);
+        String[] columns = new String[]{"Date","BUID","CampaignID","CampaignName","CampaignFlightdateStart","CampaignFlightdateEnd","AccountManagerID",
+                "CampaignStatus","AdvertiserSourceID","AdvertiserSourceName","CampaignTargetID","CampaignTargetName","CampaignTargetFlightdateStart",
+                "CampaignTargetFlightdateEnd","CampaignTargetStatus","CreativeID","CreativeName","CreativeMessageID","CreativeMessageName",
+                "AdserverPlacementID","AdserverPlacementName","IntegrationID","IntegrationName","CurrencyCode","ClickBasedConversions","ImpressionBasedConversions"};
+        mapper.setColumnMapping(columns);
+
+        CsvToBean<CreativeConversionBean> csv = new CsvToBean<CreativeConversionBean>();
+        List<CreativeConversionBean> creativeConversionList = csv.parse(mapper, new CSVReader(new FileReader(System.getProperty("user.dir") + "/" + fileName), separator));
+        List<String> creativeConversionModifiedList = new ArrayList<String>();
+        for (int i = 0; i < creativeConversionList.size(); i++) {
+            //Get csv data
+            StringBuffer lineItem = new StringBuffer();
+            lineItem.append(creativeConversionList.get(i).getDate());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getBuId());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getCampaignId());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getCampaignName());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getCampaignFlightDateStart());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getCampaignFlightDateEnd());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getAccountManagerId());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getCampaignStatus());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getAdvertiserSourceId());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getAdvertiserSourceName());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getCampaignTargetId());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getCampaignTargetName());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getCampaignTargetFlightDateStart());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getCampaignTargetFlightDateEnd());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getCampaignTargetStatus());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getCreativeId());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getCreativeName());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getCreativeMessageId());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getCreativeMessageName());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getAdserverPlacementId());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getAdserverPlacementName());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getIntegrationId());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getIntegrationName());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getCurrencyCode());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getClickBasedConversions());
+            lineItem.append(delimiter);
+            lineItem.append(creativeConversionList.get(i).getImpressionBasedConversions());
+
+            creativeConversionModifiedList.add(lineItem.toString());
+        }
+        return creativeConversionModifiedList;
     }
 }
--- src/test/resources/sql/datorama_stored_procedure.sql
+++ src/test/resources/sql/datorama_stored_procedure.sql
@@ -1,3 +1,4 @@
 dbEnvt=sqlserver
 
 thirdPartyFileGeneration_CreativeDelivery=exec [dbo].[SI_SP_ThirdPartyReportFileGeneration_CreativeDelivery] @StartDate = '$START_DATE$', @EndDate = '$END_DATE$'
+thirdPartyFileGeneration_CreativeConversion=exec [dbo].[SI_SP_ThirdPartyReportFileGeneration_CreativeConversion] @StartDate = '$START_DATE$', @EndDate = '$END_DATE$'
